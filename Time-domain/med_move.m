57973355271a655a86bb59ecf1c30be2404823e0
diegozeng <403994814@qq.com>
2013-10-06 03:13:30 +0800

    Modified med_move

--- Time-domain/med_move.m
+++ Time-domain/med_move.m
@@ -1,29 +1,29 @@
 function [] = Moveaverage(N)
 % Moving average algorithm (Low-pass characteristic)
 % N is the length of the sequence
-% 'data' is the original signal sequence
+% 'data' is the original signal sequence(1 column with ';' between elements)
 % Finally, store the results in matrix 'z'
 % 'tic' and 'toc' are used to calculate the running time of the program 
+
 tic;
 x = zeros(1,N);
-syms i j k q p;
-i = 0;
-j = 0; 
+syms i j q p; 
 k = 0;
 da = evalin('base','data');
 Len = size(da,1);
+da = da';
+da = [x,da];
 b = zeros(1,Len);
 syms count;
 syms sum;
 sum = 0;
-Len
-while(k<Len)   
-   
-
-x(N-i) = da(j+1);
-i = i+1;
-j = j+1;
- for q = 1:1:N-1
+%Len
+%It is better to use vector to replace 'for-loop' in m-file.
+for i = 1:1:Len
+    for j =1:1:N
+    x(j) = da(i + j);
+    end
+    for q = 1:1:N-1
             for p = 1:1:N-q
                 if(x(p) > x(p+1))
                     temp(p) = x(p);
@@ -31,21 +31,15 @@ j = j+1;
                     x(p+1) = temp(p);
                 end
             end
- end
-if(i == N)
-    i=0;
+    end  
+    for count = 1:1:N   
+        sum = sum + x(count);
+        a = (sum-max(x)-min(x))/(N-2);
+    end
+    sum = 0;
+    b(i) = a;
 end
-x        
-%It is better to use vector to replace 'for-loop' in m-file.
-%    for count = 1:1:N   
-%        sum = sum + x(count);
-%        a = (sum-max(x)-min(x))/(N-2);
-%    end
-%    sum = 0;
-%    b(k+1) = a;
-%    k = k + 1;
-end
-%assignin('base','z',b);
+assignin('base','z',b);
 toc;    
 
 
